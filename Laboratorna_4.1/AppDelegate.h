22c237877b83e0a45fd3089e8aaf87096b67aa6b
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2021-10-06 16:25:17 +0300

    Initial Commit

---
+++ Laboratorna_4.1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Laboratorna_4.1
+//
+//  Created by apple on 06.10.2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
